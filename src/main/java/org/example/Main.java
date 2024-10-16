50d2bce7aac08732ff6f5d137bddeb2edd0029f7
Remi26d <remidelarue11290@gmail.com>
2024-10-16 09:03:53 +0200

    2e commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    //commentaire
+    //2eme commentaire
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
