9e2a57abb7872312c05e0dca412ca1540d5dcebe
Adrien Izunsky <adrienizunsky@gmail.com>
2024-10-16 09:56:02 +0200

    modif 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     //commentaire
     //2eme commentaire
+    //modif 2
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
